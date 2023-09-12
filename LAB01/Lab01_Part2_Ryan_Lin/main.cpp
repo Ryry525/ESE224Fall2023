06d172c195f2f01c7350f1523b0b268a632b2a5a
Ryan Lin <Ryanlin0228@gmail.com>
2023-09-12 12:47:08 -0400

    Lab01Part2

---
+++ LAB01/Lab01_Part2_Ryan_Lin/main.cpp
@@ -0,0 +1,37 @@
+//Ryan Lin 114737153
+
+#include <iostream>
+#include "player.hpp"
+using namespace std;
+
+int main()
+{
+    Player player1;
+    player1.setName("Christian Ronaldo");
+    player1.setAge(38);
+    player1.setHeight(6.2);
+    player1.setNationality("Portugal");
+
+    Player player2;
+    player2.setName("Leonel Messi");
+    player2.setAge(36);
+    player2.setHeight(5.7);
+    player2.setNationality("Argentina");
+
+    cout << "Player Information:\n";
+    cout << "Name: " << player1.getName() << endl;
+    cout << "age: " << player1.getAge() << endl;
+    cout << "Height: " << player1.getHeight() << endl;
+    cout << "Nationality: " << player1.getNationality() << endl << endl;
+
+    cout << "Player2 Information:\n";
+    cout << "Name: " << player2.getName() << endl;
+    cout << "age: " << player2.getAge() << endl;
+    cout << "Height: " << player2.getHeight() << endl;
+    cout << "Nationality: " << player2.getNationality() << endl;
+
+    player2.prediction(player1);
+
+
+    return 0;
+}
---
+++ LAB01/Lab01_Part2_Ryan_Lin/player.cpp
@@ -0,0 +1,70 @@
+#include "player.hpp"
+#include <string>
+#include <iostream>
+using namespace std;
+#include <random>
+
+Player::Player()
+{
+    name = "";
+    age = 0;
+    height = 0.0;
+    nationality = "";
+}
+
+string Player::getName() const{
+    return name;
+}
+int Player::getAge() const{
+    return age;
+}
+double Player::getHeight() const{
+    return height;
+}
+string Player::getNationality() const{
+    return nationality;
+}
+
+void Player::setName(const string& newName){
+    name = newName;
+}
+void Player::setAge(int newAge){
+    age = newAge;
+}
+void Player::setHeight(double newHeight){
+    height = newHeight;
+}
+void Player::setNationality(const string& newNationality){
+    nationality = newNationality;
+}
+
+
+void Player::prediction(Player p2){
+    if (age < p2.age && height > p2.height)
+    {
+        cout << "The winner is: " << name << endl;
+    }
+    else if (age > p2.age && height < p2.height)
+    {
+        cout << "The winner is: " << p2.name << endl;
+    }
+    else 
+    {
+        random_device rd;
+        default_random_engine generator(rd());
+        uniform_real_distribution<double> distribution(0.0, 1.0);
+        double random_number = distribution(generator);
+
+        //code to choose random player that wins
+        if (random_number < 0.5)
+        {
+        cout << "The winner is: " << name << endl;
+        // cout << random_number;
+        }
+        else 
+        {
+        cout << "The winner is: " << p2.name << endl;
+        // cout << random_number;
+        }
+    }
+}
